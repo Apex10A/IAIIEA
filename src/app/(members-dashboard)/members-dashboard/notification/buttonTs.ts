102abf4d13049c8f40204cf46e3ca715de881b18
Praise Afolabi <praizafolabi@gmail.com>
2024-11-16 12:23:28 +0100

    fix/typescript errros

--- src/app/(members-dashboard)/members-dashboard/notification/buttonTs.ts
+++ src/app/(members-dashboard)/members-dashboard/notification/buttonTs.ts
@@ -8,4 +8,10 @@ export type SectionType =
   | 'Event speakers' 
   | 'Conference participants' 
   | 'Volunteers' 
+  | 'job opportunity'
+  | 'Announcement'
   | 'Workshop participants';
+
+  export type SectionState = {
+    [K in 'section1' | 'section2' | 'section3' | 'section4' | 'section5' | 'section6']: boolean;
+  };
